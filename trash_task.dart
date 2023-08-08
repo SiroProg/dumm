3e73d44dae44039d07eff148b1bd278ae9a9252e
SiroProg <070777sss@gmail.com>
2023-08-08 15:41:57 +0500

    Get a single post

--- trash_task.dart
+++ trash_task.dart
@@ -5,11 +5,11 @@ import 'package:http/http.dart';
 import 'models/post_model.dart';
 
 void main(List<String> args) async {
-  Uri uri = Uri.parse('https://dummyjson.com/posts');
+  Uri uri = Uri.parse('https://dummyjson.com/posts/1');
 
   Response response = await get(uri);
 
-  PostModel postModel = PostModel.fromJson(jsonDecode(response.body));
-  
-  print(postModel.posts[0].title);
+  Post post = Post.fromJson(jsonDecode(response.body));
+
+  print(post.title);
 }
