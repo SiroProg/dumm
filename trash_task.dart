89f03f2e530b1d9b24ad98249cda2a9246f1d25b
SiroProg <070777sss@gmail.com>
2023-08-08 16:17:21 +0500

    Delete a post

--- trash_task.dart
+++ trash_task.dart
@@ -7,9 +7,7 @@ import 'models/post_model.dart';
 void main(List<String> args) async {
   Uri uri = Uri.parse('https://dummyjson.com/posts/1');
 
-  Response response = await put(uri,
-      headers: {'Content-Type': 'application/json'},
-      body: jsonEncode({'title' : 'New title'}));
+  Response response = await delete(uri);
 
   print(response.body);
 }
