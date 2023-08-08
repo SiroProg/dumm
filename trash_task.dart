eaf2eb56305976ee3bf859e5132002c797c9b9f9
SiroProg <070777sss@gmail.com>
2023-08-08 15:43:59 +0500

    Search posts

--- trash_task.dart
+++ trash_task.dart
@@ -5,11 +5,11 @@ import 'package:http/http.dart';
 import 'models/post_model.dart';
 
 void main(List<String> args) async {
-  Uri uri = Uri.parse('https://dummyjson.com/posts/1');
+  Uri uri = Uri.parse('https://dummyjson.com/posts/search?q=love');
 
   Response response = await get(uri);
 
-  Post post = Post.fromJson(jsonDecode(response.body));
+  PostModel postModel = PostModel.fromJson(jsonDecode(response.body));
 
-  print(post.title);
+  print(postModel.total);
 }
